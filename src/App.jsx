fab07558f4c29878453f9ed56fe8f636842b6944
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2024-11-10 23:24:33 +0100

    footer done not as in the website but not important

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Collaboration from "./components/Collaboration";
 import ServicesSection from "./components/ServicesSection";
 import PricingSection from "./components/PricingSection";
 import RoadMap from "./components/RoadMap";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
           <ServicesSection />
           <PricingSection />
           <RoadMap />
+          <Footer />
         </div>
       </div>
     </>
